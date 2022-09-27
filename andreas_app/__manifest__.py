7e6b4ede04918017f24b9394fe0ab07ff0fdd9b7
aetelenetwork <aellerbrock@telenetwork.com.sv>
2022-09-27 02:07:09 +0000

    Changed author

---
+++ andreas_app/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'andreas',
+    'summary': 'Andreass Academy app to manage training',
+    'description': 'Academy module to manage training: courses, sessions, atendees',
+    'author': 'andreas',
+    'website': 'www.andreas.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': []
+}
---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'andreas',
+    'summary': 'Andreass Academy app to manage training',
+    'description': 'Academy module to manage training: courses, sessions, atendees',
+    'author': 'andreas',
+    'website': 'www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': []
+}
